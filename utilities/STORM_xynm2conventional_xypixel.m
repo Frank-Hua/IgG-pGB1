ac47543979c78df8c48b3640564bdb58b2cb84f8
FrankHua <fhuabp@gmail.com>
2017-10-20 23:00:56 -0400

    checked xy positions

--- utilities/STORM_xynm2conventional_xypixel.m
+++ utilities/STORM_xynm2conventional_xypixel.m
@@ -10,18 +10,17 @@ switch situ
     case 1
         %Use these if conventional image is 256*256 pixel^2 and xy flipped.
         fx_pos = center_y/180.0;
-        fy_pos = center_x/180.0;
+        fy_pos = center_x/180.0-1.0;
     case 2
         %Use these if conventional image is 512*512 pixel^2.
-        fx_pos = center_x/180.0+128.0;
-        fy_pos = center_y/180.0+127.0;
+        fx_pos = center_x/180.0+127.0;
+        fy_pos = center_y/180.0+128.0;
     case 3
         %Use these if conventional image is 512*512 pixel^2 and xy flipped.
         fx_pos = center_y/180.0+128.0;
         fy_pos = center_x/180.0+127.0;
     otherwise
         %Use these if conventional image is 256*256 pixel^2.
-        fx_pos = center_x/180.0;
+        fx_pos = center_x/180.0-1.0;
         fy_pos = center_y/180.0;
-
 end
